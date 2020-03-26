a603038011c7ab5463fdd7828d9d90f810bf36c8
Elsa Ventruba <you@example.com>
2020-03-26 16:09:36 +0100

    Javascript Outline

---
+++ javascript/hello.js
@@ -0,0 +1,24 @@
+// alert("Hello World");
+
+/* let message = "Hello World";
+alert(message);
+message ="Hallo Welt";
+alert(message);
+ */
+
+ const LINK_COLOR = "#ff0000"; // string
+ console.log("Link bitte in der Farbe", LINK_COLOR);
+
+let highscore = 536520;
+console.log(highscore / 12);
+
+let firstname = "John";
+let lastname = "Smith";
+console.log ("Name:", firstname, lastname);
+
+let fullname = 'Jeffrey "The Dude" Lebowski';
+console.log (fullname);
+
+let template = `Dein Heighscore sind ${highscore} Punkte`;
+console.log (template);
+
