00ca430bffd3044a2e8b7cf10e45843ea39c9ffc
Elsa Ventruba <you@example.com>
2020-04-01 14:19:14 +0200

    Leaflet 2. Versuch

--- nz/main.js
+++ nz/main.js
@@ -1,7 +1,12 @@
 //alert("Hallo Welt!")
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+
+let map = document.querySelector("#map");
+
+var mymap = L.map(map).setView([-45.874167, 170.503611], 13);
 
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-})
+}).addTo(mymap);
+
+var marker = L.marker([-45.874167, 170.503611]).addTo(mymap);
