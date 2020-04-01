5ce8b3d6ef12761f205cec7dabc7d1c05c1c782a
Elsa Ventruba <you@example.com>
2020-04-01 14:36:28 +0200

    Leaflet

--- nz/main.js
+++ nz/main.js
@@ -4,7 +4,7 @@ let map = document.querySelector("#map"); //Connection zur Karte
 let lat = map.dataset.lat;
 let lng = map.dataset.lng;
 
-var mymap = L.map(map).setView([lat,lng], 13);
+let mymap = L.map(map).setView([lat,lng], 13);
 
 let title=map.dataset.title;
 console.log(title)
@@ -13,6 +13,6 @@ console.log(title)
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-}).addTo(map);
+}).addTo(mymap);
 
 let marker = L.marker([lat,lng]).addTo(mymap);
