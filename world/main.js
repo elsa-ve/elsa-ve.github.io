b57eacce4352250275a2699e8b8a8a35ade65c1b
Elsa Ventruba <you@example.com>
2020-04-02 18:03:15 +0200

    Funktion aufrufen

--- world/main.js
+++ world/main.js
@@ -1,7 +1,7 @@
 let startLayer = L.tileLayer.provider("OpenTopoMap");
 
 let map = L.map("map", {
-    center: [50,0],
+    center: [50, 0],
     zoom: 5,
     layers: [
         L.tileLayer.provider("OpenTopoMap")
@@ -19,38 +19,40 @@ L.control.layers({
     "Esri.WorldPhysical": L.tileLayer.provider("Esri.WorldPhysical"),
 
     //add 3-4 more layers!
-},{
+}, {
     "Thematische Darstellung": circleGroup //wird dann im let circle etc etc bei .addTo(circleGroup); geändert!
 }).addTo(map);
 
 //L.marker([50,5]).addTo(map); Marker in Belgien
 
-let drawCircles = function(data) {//console.log(CONFIRMED)
+let drawCircles = function (data) {
+    //console.log(CONFIRMED)
 
-    for (let i = 1; i < CONFIRMED.length; i++) {
-        let row = CONFIRMED[i];
+    for (let i = 1; i < data.length; i++) {
+        let row = data[i];
         //console.log(row[2],row[3]);
         let reg = `${row[0]} ${row[1]}`;
         let lat = row[2];
         let lng = row[3];
-        let val = row[row.length-1];
-       // let mrk = L.marker([lat,lng]).addTo(map);
-       // mrk.bindPopup(`${reg}: ${val}`)
-    
+        let val = row[row.length - 1];
+        // let mrk = L.marker([lat,lng]).addTo(map);
+        // mrk.bindPopup(`${reg}: ${val}`)
+
         //A=r²*pi
         //r²=A/pi
         //r= wurzel(A/pi)
         //let rad =
         let s = 0.25;
-        let r =Math.sqrt(val/Math.PI);
-        let circle = L.circleMarker([lat,lng],{
-            radius: (val/2)*0.0015
+        let r = Math.sqrt(val / Math.PI);
+        let circle = L.circleMarker([lat, lng], {
+            radius: (val / 2) * 0.0015
         }).addTo(circleGroup);
         circle.bindPopup(`${reg} : ${val}`);
     };
 
 };
 
+drawCircles(CONFIRMED);
 
 
 /*console.log(CONFIRMED);
@@ -60,4 +62,3 @@ for (let i = 1; i < CONFIRMED.length; i++) {
     let mrk = L.marker([row[2],row[3]]).addTo(map);    
     mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);}
 */
-
