36d83ea291bc03f19969df14fd056f1ba450a8fc
Elsa Ventruba <you@example.com>
2020-04-02 17:04:33 +0200

    Layer hinzugefügt

--- world/main.js
+++ world/main.js
@@ -1,7 +1,21 @@
+let startLayer = L.tileLayer.provider("OpenTopoMap");
+
 let map = L.map("map", {
-    center: [0,0],
-    zoom: 2,
+    center: [50,0],
+    zoom: 5,
     layers: [
         L.tileLayer.provider("OpenTopoMap")
     ]
 });
+
+L.control.layers({
+    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
+    "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "CyclOSM": L.tileLayer.provider("CyclOSM"),
+    "OpenMapSurfer.Roads": L.tileLayer.provider("OpenMapSurfer.Roads")
+
+    //add 3-4 more layers!
+
+}).addTo(map);
+
+
