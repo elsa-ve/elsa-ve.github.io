c4c66c22a48c3a474427d3426801f75ebb8da747
Elsa Ventruba <you@example.com>
2020-04-02 18:00:27 +0200

    CircleGroup funktion

--- world/main.js
+++ world/main.js
@@ -8,6 +8,8 @@ let map = L.map("map", {
     ]
 });
 
+let circleGroup = L.featureGroup().addTo(map); //da passiert mal nichts
+
 L.control.layers({
     "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
     "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
@@ -17,34 +19,39 @@ L.control.layers({
     "Esri.WorldPhysical": L.tileLayer.provider("Esri.WorldPhysical"),
 
     //add 3-4 more layers!
-
+},{
+    "Thematische Darstellung": circleGroup //wird dann im let circle etc etc bei .addTo(circleGroup); geändert!
 }).addTo(map);
 
 //L.marker([50,5]).addTo(map); Marker in Belgien
 
-console.log(CONFIRMED)
+let drawCircles = function(data) {//console.log(CONFIRMED)
 
-for (let i = 1; i < CONFIRMED.length; i++) {
-    let row = CONFIRMED[i];
-    //console.log(row[2],row[3]);
-    let reg = `${row[0]} ${row[1]}`;
-    let lat = row[2];
-    let lng = row[3];
-    let val = row[row.length-1];
-   // let mrk = L.marker([lat,lng]).addTo(map);
-   // mrk.bindPopup(`${reg}: ${val}`)
+    for (let i = 1; i < CONFIRMED.length; i++) {
+        let row = CONFIRMED[i];
+        //console.log(row[2],row[3]);
+        let reg = `${row[0]} ${row[1]}`;
+        let lat = row[2];
+        let lng = row[3];
+        let val = row[row.length-1];
+       // let mrk = L.marker([lat,lng]).addTo(map);
+       // mrk.bindPopup(`${reg}: ${val}`)
+    
+        //A=r²*pi
+        //r²=A/pi
+        //r= wurzel(A/pi)
+        //let rad =
+        let s = 0.25;
+        let r =Math.sqrt(val/Math.PI);
+        let circle = L.circleMarker([lat,lng],{
+            radius: (val/2)*0.0015
+        }).addTo(circleGroup);
+        circle.bindPopup(`${reg} : ${val}`);
+    };
 
-    //A=r²*pi
-    //r²=A/pi
-    //r= wurzel(A/pi)
-    //let rad =
-    let s = 0.25;
-    let r =Math.sqrt(val/Math.PI);
-    let circle = L.circleMarker([lat,lng],{
-        radius: (val/2)*0.0015
-    }).addTo(map);
-    circle.bindPopup(`${reg} : ${val}`);
 };
+
+
 
 /*console.log(CONFIRMED);
 for-Schleife über alle Arrays der CONFIRMED Einträge:
