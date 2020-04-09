7599713984a61cd25480c8c4d6cdaf44a1dcb09e
Elsa Ventruba <you@example.com>
2020-04-09 15:46:13 +0200

    header und index eingefügt

--- world/main.js
+++ world/main.js
@@ -28,6 +28,8 @@ L.control.layers({
 let drawCircles = function () {
     //console.log(CONFIRMED)
     let data = CONFIRMED;
+    let header = CONFIRMED[0];
+    let index = header.length -1; //indizes von arrays immer -1!
 
     for (let i = 1; i < data.length; i++) {
         let row = data[i];
@@ -35,7 +37,7 @@ let drawCircles = function () {
         let reg = `${row[0]} ${row[1]}`;
         let lat = row[2];
         let lng = row[3];
-        let val = row[row.length - 1];
+        let val = row[index];
         // let mrk = L.marker([lat,lng]).addTo(map);
         // mrk.bindPopup(`${reg}: ${val}`)
 
