5c03940291c19c160e58c99682e8582eb90dcae8
Elsa Ventruba <you@example.com>
2020-04-16 16:14:18 +0200

    Icons

--- template_wien/main.js
+++ template_wien/main.js
@@ -24,7 +24,15 @@ L.control.layers({
 
 let walk = L.geoJson(SPAZIERGANG, {
     pointToLayer: function(point, latlng) {
-        let marker = L.marker(latlng);
+        let icon = L.icon({
+            iconUrl: 'icons/sight.svg',
+            iconSize: [32, 32]
+        });
+
+        let marker = L.marker(latlng, {
+            icon: icon
+        });
+        
         console.log("Point", point);
         marker.bindPopup(`<h3>${point.properties.NAME}</h3>
         <p><a target="links" href="${point.properties.WEITERE_INF}">Link</a></p>
@@ -32,3 +40,4 @@ let walk = L.geoJson(SPAZIERGANG, {
         return marker;
     }
 }).addTo(map);
+
