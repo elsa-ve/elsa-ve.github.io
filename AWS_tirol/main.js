6d7e81d0f66c60954f42cb88d18fbee0b1775138
Elsa Ventruba <you@example.com>
2020-04-23 17:53:43 +0200

    Color Palette Wind und Temp

--- AWS_tirol/main.js
+++ AWS_tirol/main.js
@@ -66,18 +66,36 @@ let aws = L.geoJson.ajax(awsUrl, {
     }
 }).addTo(overlay.stations);
 
+let getColor = function (val, ramp) {
+    //console.log(val, ramp);
+    let col = "red";
+
+    for (let i = 0; i < ramp.length; i++) {
+        const pair = ramp[i];
+        if (val >= pair[0]) {
+            break;
+        } else {
+            col = pair[1];
+        }
+        console.log(val, pair);
+    }
+    return col;
+};
+let color = getColor(34, COLORS.temperature);
+//console.log(color)
 
 let drawTemperature = function (jsonData) {
-    console.log("aus der Funktion", jsonData);
+    //console.log("aus der Funktion", jsonData);
     L.geoJson(jsonData, {
         filter: function (feature) {
             return feature.properties.LT;
         },
         pointToLayer: function (feature, latlng) {
+            let color = getColor(feature.properties.LT, COLORS.temperature);
             return L.marker(latlng, {
                 title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m)`,
                 icon: L.divIcon({
-                    html: `<div class="label-temperature">${feature.properties.LT.toFixed(1)}</div>`,
+                    html: `<div class="label-temperature" style="background-color: ${color}">${feature.properties.LT.toFixed(1)}</div>`,
                     className: "ignore-me" // dirty hack
                 })
             })
@@ -90,18 +108,19 @@ let drawTemperature = function (jsonData) {
 //3. neuer Stil .label-wind im css von main.css
 //4. funktion drawWind in data:loaded aufrufen
 
-let drawWind = function(jsonData) {
+let drawWind = function (jsonData) {
     //console.log("aus der Funktion", jsonData);
     L.geoJson(jsonData, {
-        filter: function(feature) {
+        filter: function (feature) {
             return feature.properties.WG;
         },
-        pointToLayer: function(feature, latlng) {
+        pointToLayer: function (feature, latlng) {
             let kmh = Math.round(feature.properties.WG / 1000 * 3600);
+            let color = getColor(kmh, COLORS.wind);
             return L.marker(latlng, {
                 title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m)`,
                 icon: L.divIcon({
-                    html: `<div class="label-wind">${kmh}</div>`,
+                    html: `<div class="label-wind" style="background-color: ${color}">${kmh}</div>`,
                     className: "ignore-me" // dirty hack
                 })
             })
@@ -109,11 +128,13 @@ let drawWind = function(jsonData) {
     }).addTo(overlay.wind);
 };
 
-aws.on("data:loaded", function() {
+aws.on("data:loaded", function () {
     //console.log(aws.toGeoJSON());
     drawTemperature(aws.toGeoJSON());
     drawWind(aws.toGeoJSON());
     map.fitBounds(overlay.stations.getBounds());
 
     overlay.wind.addTo(map);
+
+    //console.log(COLORS);
 });
