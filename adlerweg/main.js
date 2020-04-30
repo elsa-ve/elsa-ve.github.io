fee4c542ec6bab596b6af89e8a53513732d118b8
Elsa Ventruba <you@example.com>
2020-04-30 17:53:55 +0200

    Pulldown Menu

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -9,7 +9,8 @@ let map = L.map("map", {
 });
 
 let overlay = {
-    adlerblicke: L.featureGroup()
+    adlerblicke: L.featureGroup(),
+    etappen: L.featureGroup()
 };
 
 L.control.layers({
@@ -25,7 +26,8 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.overlay")
     ])
 }, {
-    "Adlerblicke": overlay.adlerblicke 
+    "Adlerblicke": overlay.adlerblicke,
+    "Adlerweg-Etappen": overlay.etappen
 }).addTo(map);
 
 //console.log(ETAPPEN);
@@ -46,25 +48,44 @@ for (const blick of ADLERBLICKE) {
 }
 overlay.adlerblicke.addTo(map);
 
-let gpx = new L.GPX("gpx/AdlerwegEtappe01.gpx", {
-    async: true,
-    marker_options: {
-        startIconUrl: 'icons/number_1.png',
-        endIconUrl: 'icons/finish.png',
-        shadowUrl: null,
-        iconSize: [32, 37],
-        iconAnchor: [16, 37], 
-        popupAnchor: [0, -37],
-      },
-    polyline_options: {
-        dashArray: [10, 5],
-        color: 'black',
-        opacity: 0.75,
-        weight: 3,
-        lineCap: 'round'
-      }
-});
+let drawEtappe = function(nr) {
+     //console.log(ETAPPEN[nr].track);
+     let track = ETAPPEN[nr].track.replace("A", "");
+     //console(track);
+
+     let gpx = new L.GPX(`gpx/AdlerwegEtappe${track}.gpx`, {
+        async: true,
+        marker_options: {
+            startIconUrl: 'icons/number_1.png',
+            endIconUrl: 'icons/finish.png',
+            shadowUrl: null,
+            iconSize: [32, 37],
+            iconAnchor: [16, 37], 
+            popupAnchor: [0, -37],
+          },
+        polyline_options: {
+            dashArray: [10, 5],
+            color: 'black',
+            opacity: 0.75,
+            weight: 3,
+            lineCap: 'round'
+          }
+    });
+
 
 gpx.on("loaded", function(evt) { 
     map. fitBounds(evt.target.getBounds());
-}).addTo(map); //so wie data.loaded, bei gpx dateien nur "loaded"
+}).addTo(overlay.etappen); //so wie data.loaded, bei gpx dateien nur "loaded"
+overlay.etappen.addTo(map);
+
+};
+drawEtappe(10);
+
+ let pulldown = document.querySelector("#pulldown");
+ console.log(pulldown);
+
+ for (let i = 0; i < ETAPPEN.length; i++) {
+     const etappe = ETAPPEN[i];
+     console.log(etappe);
+     pulldown.innerHTML +=`<option value="${i}"> ${etappe.titel}</option>`;
+ }
